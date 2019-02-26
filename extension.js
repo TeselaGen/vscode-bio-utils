32ba7aae34f9e72a28dc53e3b604647785238959
tnrich <tnrich@teselagen.com>
2019-02-26 14:20:07 -0800

    adding in all sorts of bio utils

--- extension.js
+++ extension.js
@@ -1,6 +1,15 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require("vscode");
+// const {veSequenceUtils} = require('ve-sequence-utils');
+const {
+  getAminoAcidStringFromSequenceString,
+	getComplementSequenceString,
+	getDegenerateDnaStringFromAAString,
+	getReverseComplementSequenceString,
+	calculateTm,
+	calculatePercentGC
+} = require("ve-sequence-utils");
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -9,22 +18,74 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "vscode-bio-utils" is now active!'
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-bio-utils" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.commands.registerCommand("extension.getAminoAcidStringFromSequenceString", function() {
+      // The code you place here will be executed every time your command is executed
+      replaceSelections(text => {
+        return getAminoAcidStringFromSequenceString(text);
+      });
+    })
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand("extension.getComplementSequenceString", function() {
+      // The code you place here will be executed every time your command is executed
+      replaceSelections(text => {
+        return getComplementSequenceString(text);
+      });
+    })
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand("extension.getReverseComplementSequenceString", function() {
+      // The code you place here will be executed every time your command is executed
+      replaceSelections(text => {
+        return getReverseComplementSequenceString(text);
+      });
+    })
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand("extension.getDegenerateDnaStringFromAAString", function() {
+      // The code you place here will be executed every time your command is executed
+      replaceSelections(text => {
+        return getDegenerateDnaStringFromAAString(text);
+      });
+    })
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand("extension.translate", function() {
+      // The code you place here will be executed every time your command is executed
+      replaceSelections(text => {
+        return getReverseComplementSequenceString(text);
+      });
+    })
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand("extension.calculateTm", function() {
+      // The code you place here will be executed every time your command is executed
+      replaceSelections(text => {
+        return calculateTm(text).toString();
+      });
+      return;
+    })
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand("extension.calculatePercentGC", function() {
+      // The code you place here will be executed every time your command is executed
+      replaceSelections(text => {
+        return calculatePercentGC(text).toString();
+      });
+      return;
+    })
+  );
 }
 exports.activate = activate;
 
@@ -32,6 +93,20 @@ exports.activate = activate;
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
+  activate,
+  deactivate
+};
+
+function replaceSelections(fn) {
+  var editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    return; // No open text editor
+  }
+  editor.edit(editBuilder => {
+    vscode.window.activeTextEditor.selections.forEach(selection => {
+      var text = editor.document.getText(selection);
+      const toReplace = fn(text);
+      editBuilder.replace(selection, toReplace);
+    });
+  });
 }
