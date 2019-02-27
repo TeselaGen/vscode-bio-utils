03bf3680fa090e6254710d67ae48b382f6848340
tnrich <tnrich@teselagen.com>
2019-02-27 10:50:43 -0800

    adding .fasta/.gb/.json/.bed/.dna conversion utils

--- extension.js
+++ extension.js
@@ -3,12 +3,21 @@
 const vscode = require("vscode");
 // const {veSequenceUtils} = require('ve-sequence-utils');
 const {
+  genbankToJson,
+  jsonToFasta,
+	jsonToGenbank,
+	fastaToJson,
+  jsonToBed,
+  snapgeneToJson
+} = require("bio-parsers");
+
+const {
   getAminoAcidStringFromSequenceString,
-	getComplementSequenceString,
-	getDegenerateDnaStringFromAAString,
-	getReverseComplementSequenceString,
-	calculateTm,
-	calculatePercentGC
+  getComplementSequenceString,
+  getDegenerateDnaStringFromAAString,
+  getReverseComplementSequenceString,
+  calculateTm,
+  calculatePercentGC
 } = require("ve-sequence-utils");
 
 // this method is called when your extension is activated
@@ -29,36 +38,113 @@ function activate(context) {
   // The commandId parameter must match the command field in package.json
 
   context.subscriptions.push(
-    vscode.commands.registerCommand("extension.getAminoAcidStringFromSequenceString", function() {
-      // The code you place here will be executed every time your command is executed
-      replaceSelections(text => {
-        return getAminoAcidStringFromSequenceString(text);
-      });
-    })
+    vscode.commands.registerCommand(
+      "extension.convertSnapgeneToGenbank",
+      function() {
+        // The code you place here will be executed every time your command is executed
+        return replaceSelectionOrFilePromise(text => {
+          return snapgeneToJson(text).then(results => {
+            return jsonToGenbank(results[0].parsedSequence);
+          });
+        });
+      }
+    )
   );
   context.subscriptions.push(
-    vscode.commands.registerCommand("extension.getComplementSequenceString", function() {
-      // The code you place here will be executed every time your command is executed
-      replaceSelections(text => {
-        return getComplementSequenceString(text);
-      });
-    })
+    vscode.commands.registerCommand(
+      "extension.convertFastaToGenbank",
+      function() {
+        // The code you place here will be executed every time your command is executed
+        return replaceSelectionOrFilePromise(text => {
+          return fastaToJson(text).then(results => {
+            return jsonToGenbank(results[0].parsedSequence);
+          });
+        });
+      }
+    )
   );
   context.subscriptions.push(
-    vscode.commands.registerCommand("extension.getReverseComplementSequenceString", function() {
-      // The code you place here will be executed every time your command is executed
-      replaceSelections(text => {
-        return getReverseComplementSequenceString(text);
-      });
-    })
+    vscode.commands.registerCommand(
+      "extension.convertGenbankToTeselagenJson",
+      function() {
+        // The code you place here will be executed every time your command is executed
+        return replaceSelectionOrFilePromise(text => {
+          return genbankToJson(text).then(results => {
+            return JSON.stringify(results[0].parsedSequence, null, 2);
+          });
+        });
+      }
+    )
   );
   context.subscriptions.push(
-    vscode.commands.registerCommand("extension.getDegenerateDnaStringFromAAString", function() {
-      // The code you place here will be executed every time your command is executed
-      replaceSelections(text => {
-        return getDegenerateDnaStringFromAAString(text);
-      });
-    })
+    vscode.commands.registerCommand(
+      "extension.convertGenbankToFasta",
+      function() {
+        // The code you place here will be executed every time your command is executed
+        return replaceSelectionOrFilePromise(text => {
+          return genbankToJson(text).then(results => {
+            return jsonToFasta(results[0].parsedSequence);
+          });
+        });
+      }
+    )
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "extension.convertGenbankToBed",
+      function() {
+        // The code you place here will be executed every time your command is executed
+        return replaceSelectionOrFilePromise(text => {
+          return genbankToJson(text).then(results => {
+            return jsonToBed(results[0].parsedSequence);
+          });
+        });
+      }
+    )
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "extension.getAminoAcidStringFromSequenceString",
+      function() {
+        // The code you place here will be executed every time your command is executed
+        replaceSelections(text => {
+          return getAminoAcidStringFromSequenceString(text);
+        });
+      }
+    )
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "extension.getComplementSequenceString",
+      function() {
+        // The code you place here will be executed every time your command is executed
+        replaceSelections(text => {
+          return getComplementSequenceString(text);
+        });
+      }
+    )
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "extension.getReverseComplementSequenceString",
+      function() {
+        // The code you place here will be executed every time your command is executed
+        replaceSelections(text => {
+          return getReverseComplementSequenceString(text);
+        });
+      }
+    )
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "extension.getDegenerateDnaStringFromAAString",
+      function() {
+        // The code you place here will be executed every time your command is executed
+        replaceSelections(text => {
+          return getDegenerateDnaStringFromAAString(text);
+        });
+      }
+    )
   );
   context.subscriptions.push(
     vscode.commands.registerCommand("extension.translate", function() {
@@ -107,6 +193,29 @@ function replaceSelections(fn) {
       var text = editor.document.getText(selection);
       const toReplace = fn(text);
       editBuilder.replace(selection, toReplace);
+    });
+  });
+}
+function replaceSelectionOrFilePromise(fn) {
+  var editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    return; // No open text editor
+  }
+  var { selection } = vscode.window.activeTextEditor;
+  var text;
+  if (selection && !selection.isEmpty) {
+    text = editor.document.getText(selection);
+  } else {
+    text = editor.document.getText();
+    selection = new vscode.Range(
+      editor.document.positionAt(0),
+      editor.document.positionAt(text.length - 1)
+    );
+  }
+
+  fn(text).then(val => {
+    editor.edit(editBuilder => {
+      editBuilder.replace(selection, val);
     });
   });
 }
